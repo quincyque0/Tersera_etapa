9e2836cd6d8919cb0a8d7b73c69922502d9fa848
quincyque0 <quincyque0@gmail.com>
2026-02-27 12:46:25 +0700

    Delete build directory

--- build/FindZeroMQ.cmake
+++
@@ -1,8 +0,0 @@
-# Принудительно находим ZeroMQ
-set(ZeroMQ_FOUND TRUE)
-set(ZeroMQ_INCLUDE_DIR "/usr/local/opt/zeromq/include")
-set(ZeroMQ_LIBRARY "/usr/local/opt/zeromq/lib/libzmq.dylib")
-set(ZEROMQ_INCLUDE_DIR "/usr/local/opt/zeromq/include")
-set(ZEROMQ_LIBRARY "/usr/local/opt/zeromq/lib/libzmq.dylib")
-set(ZeroMQ_INCLUDE_DIRS "/usr/local/opt/zeromq/include")
-set(ZeroMQ_LIBRARIES "/usr/local/opt/zeromq/lib/libzmq.dylib")
